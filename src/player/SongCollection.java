8618a4da7df56ea468ea78861912add45228029f
Olivia Smith <31713886+liv-yaa@users.noreply.github.com>
2018-09-22 13:56:15 -0700

    commit sc

--- src/player/SongCollection.java
+++ src/player/SongCollection.java
@@ -1,62 +1,123 @@
 package player;
 
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
-/**
- * The Driver class for part 1 of lab 2, the text-based mp3 player.
- * Creates SongCollection using songs from the directory specified by args[0].
- * Interacts with the user allowing the user to find songs by artist,
- * then by title and play them.
+/** SongCollection class stores all the songs in a nested TreeMap (see below),
+ *  and has methods to manipulate songs.
  *
- *
- * I am going off my previous work with the LibraryCatalog user interface... - OS
+ *      // A TreeMap, where each key is an artist, and the value is another TreeMap,
+ *     // where each key is the title of the song written by this artist, and the value
+ *     // is the reference to the corresponding Song. Note: this data structure will make
+ *     // the search efficient when the user first searches by artist, then by title.
+ *     // Since it's a TreeMap, all keys will be sorted alphabetically.
  */
-public class MPlayer {
-    public static void main(String[] args) {
-        SongCollection songs = new SongCollection(); // Constructor makes an empty 2D TreeMap too.
-        if (args.length == 0) {
-            System.out.println("No argument provided");
-            return; // Why does she have this here?
-        }
-
-        // FILL IN CODE
-        else {
-
-            // Get from a directory
-            // String directory = new dir;
-            // sc.loadSongs(directory);
-            songs.loadSongs(args[0]); // args have been set to:  dir
-
-            // User Input / Validation section
-
-            Scanner scanner = new Scanner(System.in);
-            boolean valid = true; // Made a bool to control the following do-while statement:
-
-            do {
-                System.out.println("Welcome to the MP3 Player. Please enter your song title: ");
-                String songTitle = scanner.next();
 
 
-                if (// Found song title){
+// For first draft, I am going to add some methods similar to the LibraryCatalog class
 
-                    System.out.println("Song title found. Playing song...");
-                    // Play method here
+public class SongCollection {
 
-                //}
-                else {
-                    System.out.println("Song title not found. Try again: ");
-                    // Loop again. Check it
-                }
+    // In a TreeMap, each key is the name of the artist, and the value is a nested TreeMap, where each key
+    // is the title of the song written by this artist, and the value is the reference to the corresponding Song.
+    private Map<String, Map<String, Song>> songs;
 
 
-
-            } while (valid);
-
-
-
-
-
-        }
+    /** Constructor of class SongCollection
+     * */
+    public SongCollection() {
+        songs = new TreeMap<>();
 
     }
+
+    /** Finds all .mp3 files in a given directory, creates Songs from them
+     * and adds them to this collection
+     * Note you are not allowed to use class File; You are required to use
+     * classes from nio package we discussed in class.
+     * @param dir Name of the project directory that contains mp3 files
+     *
+     *            They should be in order of artist (key)
+     */
+    public void loadSongs(String dir) {
+        // FILL IN CODE
+        // Copying from the DirectoryStreamExample.java (Karpenko's example)
+        // Gets all files and subdirectories in a given directory
+
+        Path path = Paths.get(dir); // Made a new path, arg is the String directory name
+        try (DirectoryStream<Path> filesList = Files.newDirectoryStream(path)) {
+            for (Path file : filesList) {
+                if (!(Files.isDirectory(file))) { // Found the file in the dir
+                    System.out.println("Creating song object from " + file.toString());
+
+                    // Trying to create new Path object from file in FilesList
+                    // This was a wonky way to make it become a string but I want to double check it
+                    Song song = new Song(file.getFileName().toString()); // Song(String filename)
+
+                    System.out.println("Song object created " + song.toString());
+
+                    // Add song object to the collection (TreeMap):
+                    // First we want to find all songs with given artist and make a Map<String, Song>:
+                    // Made a new Map to store Title, Song:
+                    Map<String, Song> titleSongMap = new Map<String, Song>;
+                    titleSongMap.put(song.getTitle(), song);
+
+                    // This should be at a higher level
+                    songs.put(song.getArtist(), titleSongMap);
+
+
+
+
+                }
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Cannot open directory.");
+        }
+    }
+
+    // FILL IN CODE
+    // Add other methods as needed - before you start coding, think of what
+    // methods you want to have in this class
+
+     /** @param title title of the song
+     * @return reference to the Song with the given title
+      *
+      * Searches through the TreeMap (aka songs)
+      * I made this - OS
+      * */
+    public Song findSongTitle(String title) {
+
+        for (Song s: songs) {
+            if (s.getTitle().equals(title)) {
+                return s;
+            }
+        }
+        // If we cannot find song,
+        return null;
+    }
+
+    /** @param artist of the song
+     * @return reference to the Song with the given artist
+     *
+     * Searches through the TreeMap (aka songs)
+     * I made this - OS
+     * */
+    public Song findSongArtist(String artist) {
+        for (Song s: songs) {
+            if (s.getArtist().equals(artist)) {
+                return s;
+            }
+        }
+        // If we cannot find song,
+        return null;
+    }
+
 }
