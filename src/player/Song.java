3e1ff8c050215199f791f30ca43eea31fe72347a
Olivia Smith <31713886+liv-yaa@users.noreply.github.com>
2018-09-23 09:30:35 -0700

    Player works! Everything works :)

--- src/player/Song.java
+++ src/player/Song.java
@@ -10,9 +10,7 @@ import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
 import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
 import org.jaudiotagger.tag.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 // is java.nio is preferred for this project? Contains Path, Paths, Files, FileSystem, DirectorySystem...
 
@@ -24,9 +22,7 @@ public class Song  {
     // FILL IN CODE: add instance variables
     private String title;
     private String artist;
-    // take out player - you can access it as a local variable.
-    private AdvancedPlayer player; // Was it incorrect to add this additional private var? the reason was I used it in two methods...
-    private String filename; // Was it incorrect to add this additional private var?
+    private String filename;
 
     /** Constructor of class Song.
      * Takes the name of the file that contains the mp3 of this song,
@@ -39,7 +35,7 @@ public class Song  {
         // FILL IN CODE
         this.filename = filename;
 
-// GOOD EXAMPLE omg :::: https://www.programcreek.com/java-api-examples/?api=org.jaudiotagger.audio.AudioFile
+        // Src :::: https://www.programcreek.com/java-api-examples/?api=org.jaudiotagger.audio.AudioFile
 
         try {
 
@@ -52,28 +48,23 @@ public class Song  {
             title = tag.getFirst(FieldKey.TITLE);
             artist = tag.getFirst(FieldKey.ARTIST);
 
-            // CHECK THAT THIS IS CORRECT...making an Advanced Player object?? Also, it is private. I need to really makes ure that is ok.
-            // Source was just...
-            // https://stackoverflow.com/questions/26946826/use-jlayer-to-play-mp3-resource
-            FileInputStream fis = new FileInputStream(filename);
-            player = new AdvancedPlayer(fis);
+//            // https://stackoverflow.com/questions/26946826/use-jlayer-to-play-mp3-resource
+//            FileInputStream fis = new FileInputStream(filename);
+//            // player = new AdvancedPlayer(fis);
 
-        } catch (JavaLayerException | IOException | TagException | CannotReadException | ReadOnlyFileException | InvalidAudioFrameException e) {
+        } catch ( IOException | TagException | CannotReadException | ReadOnlyFileException | InvalidAudioFrameException e) {
             System.out.println("Some exception occurred when reading from file.");
             e.printStackTrace();
         }
-
     }
 
-
     /** Returns a string representation of the Song: title and artist separated
-     * by " by ", for instance:
-     * Hot N Cold by Katy Perry
+     * by " by ", for instance: Hot N Cold by Katy Perry
      * @return string representing the song
      */
     public String toString() {
-
         // FILL IN CODE
+
         String res = title + " by " + artist;
         return res;
     }
@@ -81,26 +72,24 @@ public class Song  {
     /**
      * Plays the song using Player class of jl library. JLayer is a Java library that allows MP3 files to be played from Java programs.
      * See project description for details.
-     * Please note: this method will likely not be in this class
-     * for part 2 of the project. But for part 1 it makes sense to
+     * Please note: this method will likely not be in this class for part 2 of the project. But for part 1 it makes sense to
      * have it in this class
      *
+     * Here's the example I used - OS:
+     * https://www.programcreek.com/java-api-examples/?api=javazoom.jl.player.Player
      */
-//     http://www.javazoom.net/javalayer/docs/docs1.0/javazoom/jl/player/Player.html I saw:
-//     Constructor Summary
-//     Player(java.io.InputStream stream)
-//
-//     https://www.programcreek.com/java-api-examples/index.php?source_dir=JMediaPlayer-master/JLayer1.0.1/src/javazoom/jl/player/jlp.java
-//     Example with making Player object
 
     public void play() {
         // FILL IN CODE
-        Player player = new Player;  // using Player class of jl library http://www.javazoom.net/javalayer/docs/docs1.0/javazoom/jl/player/Player.html
-        // Needs to take a stream
 
         try {
+            FileInputStream fis = new FileInputStream(filename);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+
+            Player player = new Player(bis);
             player.play();
-        } catch (JavaLayerException e) {
+
+        } catch (JavaLayerException | FileNotFoundException e) {
             e.printStackTrace();
         }
 
@@ -122,8 +111,10 @@ public class Song  {
     }
 
     public static void main(String[] args) {
-        Song s = new Song("/Users/liv/IdeaProjects/lab2-liv-yaa/songs/SFW40517_06.mp3");
+        Song s = new Song("dir/Jai Paul - BTSTU (Edit).mp3");
         System.out.println(s);
+
+        s.play();
     }
 
 }
