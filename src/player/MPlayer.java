fc61193b3210f540662391609fe2caf2183f8c61
Olivia Smith <31713886+liv-yaa@users.noreply.github.com>
2018-10-04 11:06:40 -0700

    Split main() into 2 fxns

--- src/player/MPlayer.java
+++ src/player/MPlayer.java
@@ -2,6 +2,7 @@ package player;
 
 import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
@@ -28,68 +29,81 @@ public class MPlayer {
         // FILL IN CODE
         else {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Please enter the directory containing the .mp3 files: ");
-            String directory = scanner.nextLine();
-            songs.loadSongs(directory); // User should enter : dir (or other directory)
-
-            String artistName;
-
             while (true) {
-                System.out.println(songs);
-                System.out.println("Search the song by the artist or press Q to exit: ");
-                artistName = scanner.nextLine();
-
-                if (artistName.matches("Q")) {
-                    System.out.println("Goodbye");
+                System.out.println("Please enter the directory containing the .mp3 files: ");
+                String directory = scanner.nextLine();
+                if (!(directory.matches("dir"))) {
+                    System.out.println("For this project, please enter \"dir\".");
+                }
+                else {
+                    songs.loadSongs(directory); // User should enter : dir (or other directory)
                     break;
-
-                } else if (songs.artistNameExists(artistName)) { // If artist exists...
-
-                    ArrayList<String> artistList = songs.getArtistList(artistName); // Make an ArrayList of all the names
-
-                    System.out.println("These are all the songs by this artist. Which one would you like to play?");
-                    for (String artName : artistList) {
-                        ArrayList<String> songList = songs.getSongTitleList(artName);
-
-                        for (String songName : songList) {
-                            System.out.println("- " + songName);
-                        }
-                    }
-
-                    boolean askForSong = false;
-                    do {
-                        String songName = scanner.nextLine();
-
-                        if (songs.getSong(artistName, songName) == null) {
-
-                            System.out.println("You entered an invalid song name " + songName + ". Please try again: ");
-                            askForSong = true;
-
-                        } else {
-
-                            try {
-                                String stop;
-                                do {
-                                    Song foundSong = songs.getSong(artistName, songName);
-                                    System.out.println("Playing the song: " + foundSong.toString());
-                                    foundSong.play();
-
-                                    System.out.println("To stop, enter 'S'");
-                                    Scanner sc = new Scanner(System.in);
-                                    stop = sc.nextLine();
-
-                                } while (stop.matches("S"));
-
-                            } catch (NullPointerException e) {
-                                System.out.println("Cannot play song.");
-                            }
-                        }
-                    } while (askForSong);
-                } else {
-                    System.out.println("Artist not found. Try again: ");
                 }
             }
+            promptUser(songs);
         }
+    }
+
+    static void promptUser(SongCollection songCol) {
+        String artistName;
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(songCol);
+            System.out.println("Search the song by the artist or press Q to exit: ");
+            artistName = scanner.nextLine();
+
+            if (artistName.matches("Q")) {
+                System.out.println("Goodbye");
+                break;
+
+            } else if (songCol.artistNameExists(artistName)) { // If artist exists...
+
+                ArrayList<String> artistList = songCol.getArtistList(artistName); // Make an ArrayList of all the names
+
+                System.out.println("These are all the songs by this artist. Which one would you like to play?");
+                for (String artName : artistList) {
+                    ArrayList<String> songList = songCol.getSongTitleList(artName);
+
+                    for (String songName : songList) {
+                        System.out.println("- " + songName);
+                    }
+                }
+
+                boolean askForSong = false;
+                do {
+                    String songName = scanner.nextLine();
+
+                    if (songCol.getSong(artistName, songName) == null) {
+
+                        System.out.println("You entered an invalid song name " + songName + ". Please try again: ");
+                        askForSong = true;
+
+                    } else {
+
+                        try {
+                            String stop;
+                            do {
+                                Song foundSong = songCol.getSong(artistName, songName);
+                                System.out.println("Playing the song: " + foundSong.toString());
+                                foundSong.play();
+
+                                System.out.println("To stop, enter 'S'");
+                                Scanner sc = new Scanner(System.in);
+                                stop = sc.nextLine();
+
+                            } while (stop.matches("S"));
+
+                        } catch (NullPointerException e) {
+                            System.out.println("Cannot play song.");
+                        }
+                    }
+                } while (askForSong);
+            } else {
+                System.out.println("Artist not found. Try again: ");
+            }
+        }
+
     }
 }
 
